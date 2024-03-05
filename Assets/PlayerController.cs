769b63269f530c761e031c76a5dc6c4df90d2ef1
Patrick King <pat.k195@gmail.com>
2024-03-05 13:50:14 -0500

    Added a new player model with an idle/walking animation

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -24,17 +24,35 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate() {
         // If movement input is not 0 try to move
         if (movementInput != Vector2.zero) {
-            int count = rb.Cast(
-                movementInput,
-                movementFilter,
-                castCollisions,
-                moveSpeed * Time.fixedDeltaTime + collisionOffset);
-            
-            if (count == 0) {
-                rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+            bool success = TryMove(movementInput);
+
+            if (!success) {
+                success = TryMove(new Vector2(movementInput.x, 0));
+
+                if (!success) {
+                    success = TryMove(new Vector2(0, movementInput.y));
+                }
             }
+
         }
     }
+
+    private bool TryMove(Vector2 direction) {
+        // Check for potential collisions
+        int count = rb.Cast(
+            direction, // X andy Y values between -1 and 1 that represent the direction from the body to look for collisions
+            movementFilter, // The settings that determine where a collision can occur
+            castCollisions, // List of collisions to store the found collisions into after the cast is finished
+            moveSpeed * Time.fixedDeltaTime + collisionOffset); // The amount to cast equal to the movement plus an offset
+        
+        // If nothing exists in the players way they can Move!
+        if (count == 0) {
+            rb.MovePosition(rb.position + moveSpeed * Time.fixedDeltaTime * direction);
+            return true;
+        } else {
+            return false;
+        }
+    } 
 
     void OnMove(InputValue movementValue) {
         movementInput = movementValue.Get<Vector2>();
